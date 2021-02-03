18c5372a21bcaf535a04c7d79507dd2f1cef2ed5
daniyar.seferov <daniyar.seferov@rockdevels.com>
2021-02-03 10:39:54 +0300

    #Task 1: Functions - bug fix

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 'use strict';
 
+const validateRandomRange = (min, max) => {
+  if (min < 0 || max < 0) {
+    throw new Error('Parameters must be greater than or equal to zero.')
+  }
+  else if (min > max) {
+    throw new Error('min value must be less than max.');
+  }
+}
+
 /**
  * Generates a random integer number from min to max including both min and max as possible values.
  * Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random#getting_a_random_integer_between_two_values_inclusive
@@ -9,12 +18,7 @@
  * @returns {number}
  */
 const getRandomIntInclusive = (min, max) => {
-  if (min < 0 || max < 0) {
-    throw new Error('Parameters must be greater than or equal to zero.')
-  }
-  else if (min > max) {
-    throw new Error('min value must be less than max.');
-  }
+  validateRandomRange(min, max);
 
   min = Math.ceil(min);
   max = Math.floor(max);
@@ -31,12 +35,7 @@ const getRandomIntInclusive = (min, max) => {
  * @returns {number}
  */
 const getRandomFloatInclusive = (min, max, decimals = 0) => {
-  if (min < 0 || max < 0) {
-    throw new Error('Parameters must be greater than or equal to zero.')
-  }
-  else if (min > max) {
-    throw new Error('min value must be less than max.');
-  }
+  validateRandomRange(min, max);
 
   let rand = Math.random() * (max - min) + min;
   return Number(Math.floor(rand + 'e' + decimals) + 'e-' + decimals);
