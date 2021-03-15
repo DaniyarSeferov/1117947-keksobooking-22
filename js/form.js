24a38ef9ad5a5b72586fb1b6095a72b665768db5
daniyar.seferov <daniyar.seferov@rockdevels.com>
2021-03-15 11:53:39 +0300

    #Task 6.2: Form validation

--- js/form.js
+++ js/form.js
@@ -5,6 +5,9 @@ const typeElement = adFormElement.querySelector('#type');
 const priceElement = adFormElement.querySelector('#price');
 const timeInElement = adFormElement.querySelector('#timein');
 const timeOutElement = adFormElement.querySelector('#timeout');
+const titleElement = adFormElement.querySelector('#title');
+const roomNumberElement = adFormElement.querySelector('#room_number');
+const capacityElement = adFormElement.querySelector('#capacity');
 
 const setPriceElementData = (value) => {
   const minPrice = getAccomodationMinPrice(value);
@@ -24,4 +27,44 @@ timeInElement.addEventListener('change', (event) => {
 
 timeOutElement.addEventListener('change', (event) => {
   timeInElement.value = event.target.value;
+});
+
+titleElement.addEventListener('invalid', () => {
+  if (titleElement.validity.tooShort) {
+    titleElement.setCustomValidity('Имя должно состоять минимум из 30-ти символов');
+  } else if (titleElement.validity.tooLong) {
+    titleElement.setCustomValidity('Имя не должно превышать 100 символов');
+  } else if (titleElement.validity.valueMissing) {
+    titleElement.setCustomValidity('Обязательное поле');
+  } else {
+    titleElement.setCustomValidity('');
+  }
+});
+
+const validateRoomCapacity = () => {
+  let error = '';
+  const capacity = Number(capacityElement.value);
+  const roomNumber = Number(roomNumberElement.value);
+
+  if (roomNumber === 100 && capacity !== 0) {
+    error = 'Только не для гостей';
+  } else if (roomNumber !== 100 && capacity === 0) {
+    error = 'Только 100 комнат';
+  } else if (capacity > roomNumber) {
+    error = 'Количество гостей не должно превышать количество комнат.';
+  }
+
+  return error;
+}
+
+roomNumberElement.addEventListener('input', () => {
+  const error = validateRoomCapacity();
+  roomNumberElement.setCustomValidity(error);
+  roomNumberElement.reportValidity();
+});
+
+capacityElement.addEventListener('input', () => {
+  const error = validateRoomCapacity();
+  capacityElement.setCustomValidity(error);
+  capacityElement.reportValidity();
 });
